b5649652f86fb6e4f23094a8f265458229706d15
Male Diaz Falvo <malenadiazfalvo@gmail.com>
2023-09-14 22:56:59 -0300

    Ej 3 funcionando

--- emparejar.py
+++ emparejar.py
@@ -2,7 +2,15 @@ import csv
 import sys
 import math
 
-def Ordenar_Contar(L, n):
+def es_menor(a, b, ref):
+    ia, ib = 0, 0
+    while ref[ia] is not a:
+        ia += 1
+    while ref[ib] is not b:
+        ib += 1
+    return ia < ib
+
+def ordenar_contar(L, n, ref):
     if n <= 1:
         return 0, L
     else:
@@ -12,25 +20,25 @@ def Ordenar_Contar(L, n):
         B = L[:mid_b]
         T = L[mid_t:]
 
-        inv_b, B = Ordenar_Contar(B, mid_b)
-        inv_t, T = Ordenar_Contar(T, n - mid_t)
+        inv_b, B = ordenar_contar(B, mid_b, ref)
+        inv_t, T = ordenar_contar(T, n - mid_t, ref)
 
-        inv, L = Merge_Contar(B, mid_b, T, n - mid_t)
+        inv, L = merge_contar(B, mid_b, T, n - mid_t, ref)
 
     return inv_b + inv_t + inv, L
 
-def Merge_Contar(A, n_a, B, n_b):
+def merge_contar(A, n_a, B, n_b, ref):
     L = [0 for _ in range(n_a + n_b)]
     inv, i, j = 0, 0, 0
     
     while i < n_a and j < n_b:
         a, b = A[i], B[j]
-        if a < b:
+        if es_menor(a, b, ref):
             L[i + j] = a
             i += 1
         else:
             L[i + j] = b
-            inv += n_a - i
+            inv += (n_a - i)
             j += 1
 
     while j < n_b:
@@ -50,17 +58,17 @@ alumnos_csv = csv.reader(alumnos_txt, delimiter=",")
 alumnos = [row for row in alumnos_csv]
 nombres = [row[0] for row in alumnos]
 conocimientos = [row[1:] for row in alumnos]
+n = len(alumnos)
 comp = 0
 
-capitan = Ordenar_Contar(conocimientos[capitan_i], 8)
 nombre = None
+comp = 0
 
-for i in range(0, 5):
+for i in range(0, n):
     if i != capitan_i:
-        aux = Ordenar_Contar(conocimientos[i], 8)
-        if abs(aux[0] - capitan[0]) > comp:
-            print(aux[0] - capitan[0], comp, capitan[0])
-            comp = abs(aux[0] - capitan[0])
+        aux = ordenar_contar(conocimientos[i], 8, conocimientos[capitan_i])
+        if aux[0] > comp:
+            comp = aux[0]
             complementario = aux
             nombre = nombres[i]
 
