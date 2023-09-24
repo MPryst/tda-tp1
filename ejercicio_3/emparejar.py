0439e95f52b1509ae2de7c405be1b4d6f3dfcd0b
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2023-09-24 16:59:26 -0300

    Validacion en argumento de capitan

--- ejercicio_3/emparejar.py
+++ ejercicio_3/emparejar.py
@@ -60,6 +60,10 @@ def main():
     categorias = [row[1:] for row in alumnos]
     n = len(alumnos)
 
+    if capitan_i >= len(alumnos) or capitan_i <0:
+        print('Error: El numero ingresado no corresponde a una fila de alumno')
+        exit(code=1)
+
     ref = {}
     for i, c in enumerate(categorias[capitan_i]):
         ref[c] = i
